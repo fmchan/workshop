ff84b186fee132be7d1ee07edcea94dce7c887cb
fmchan <formingchan@gmail.com>
2015-09-02 16:57:33 +0800

    Module 10: Implementing View Routing

--- www/js/app.js
+++ www/js/app.js
@@ -6,12 +6,25 @@
     service.initialize().done(function () {
         console.log("Service initialized");
         //renderHomeView();
-        $('body').html(new HomeView(service).render().$el);
+        //$('body').html(new HomeView(service).render().$el);
+
+        router.addRoute('', function() {
+            $('body').html(new HomeView(service).render().$el);
+        });
+
+        router.addRoute('employees/:id', function(id) {
+            service.findById(parseInt(id)).done(function(employee) {
+                $('body').html(new EmployeeView(employee).render().$el);
+            });
+        });
+
+        router.start();
     });
     //var homeTpl = Handlebars.compile($("#home-tpl").html());
     //var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
     HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
     EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html());
+    EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
     /* --------------------------------- Event Registration -------------------------------- */
     //$('.search-key').on('keyup', findByName);
     /*$('.help-btn').on('click', function() {
